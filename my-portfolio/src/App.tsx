7daf0895d783596da1574f4adc018e705c6e4bf2
Yerai Araujo <yeraiaraujo227@gmail.com>
2025-03-26 00:49:28 +0100

    New portofolio using React and TypeScript

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,39 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='header'>
+        <h1>Yerai Araujo</h1>
+        <img src="src/assets/images/my_cv_img.jpg" alt="Profile Picture" className="profile-picture" />
+      </div>
+      <div className='content'>
+        <h2>About me</h2>
+        <p>
+          💻 I am a Software Engineer passionate about software development.<br />
+          🎓 I have a degree in Software Engineering from the Polytechnic University of Madrid.<br />
+          🔍 Looking for job opportunities.<br />
+          🛠️ Skills: Java, SQL, Spring, Python, TypeScript, JavaScript, React, Html and Css.<br />
+          I have work experience in web application development using technologies such as Spring, React and SQL.<br />
+          I have been working almost a year as a Junior Java Developer in a company.<br />
+          I am a fast learner and I am always looking for new challenges to improve my skills.
+        </p>
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <a href="">More information about my projects</a>
+          </li>
+          </ul>
+        <h2>Contact</h2>
+        <p>
+          Email: <strong>yeraiaraujo227@gmail.com</strong><br />
+          GitHub: <a href="https://github.com/Yerai-Araujo">https://github.com/Yerai-Araujo</a> <br />
+          LinkedIn: <a href="https://www.linkedin.com/in/yerai-araujo/">https://www.linkedin.com/in/yerai-araujo/</a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/assets/database/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO webappdb.product VALUES (NULL, 'Pizza', 20);
+INSERT INTO webappdb.product VALUES (NULL, 'Burger', 15);
+INSERT INTO webappdb.product VALUES (NULL, 'Taco', 10);
+
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Bacon', 3);
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Egg', 1.5);
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Cheese', 2);
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Tuna', 3);
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Pepperoni', 1.5);
+INSERT INTO webappdb.ingredient VALUES (NULL, 'Chicken', 2.5);
