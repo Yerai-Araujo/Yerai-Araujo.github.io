faa2245f05602ccbf88d0bf6c8d1366a83d61c3e
Yerai Araujo <yeraiaraujo227@gmail.com>
2025-03-26 01:00:35 +0100

    First deploy of the new portfolio

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // Add this line to set the base path
   plugins: [react()],
 })
